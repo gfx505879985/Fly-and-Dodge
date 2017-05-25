7971c59cd4139af7a2e11ddd2f0172fd08870ede
Fangxian Gu <gfx505879985@gmail.com>
2017-05-24 23:12:16 -0400

    Add files via upload

---
+++ scripts/asteroid.js
@@ -0,0 +1,24 @@
+var Asteroid = pc.createScript('asteroid');
+
+// initialize code called once per entity
+Asteroid.prototype.initialize = function() {
+    
+    this.camera = this.app.root.findByName('GameCamera');
+};
+
+// update code called every frame
+Asteroid.prototype.update = function(dt) {
+    this.relax();
+};
+
+Asteroid.prototype.relax = function()
+{
+    if(this.entity.getPosition().z>=(this.camera.getPosition().z-15))
+        this.entity.destroy();
+};
+// swap method called for script hot-reloading
+// inherit your script state here
+// Asteroid.prototype.swap = function(old) { };
+
+// to learn more about script anatomy, please read:
+// http://developer.playcanvas.com/en/user-manual/scripting/
